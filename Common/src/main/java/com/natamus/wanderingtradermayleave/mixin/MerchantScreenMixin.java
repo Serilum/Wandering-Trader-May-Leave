9041c8408c7b4a66627846e00c3ad5c892fc92f5
Rick South <rick@natamus.com>
2025-06-30 13:55:14 +0200

    (v1.1) Fixed the leave button appearing on normal villager trade screens.

--- Common/src/main/java/com/natamus/wanderingtradermayleave/mixin/MerchantScreenMixin.java
+++ Common/src/main/java/com/natamus/wanderingtradermayleave/mixin/MerchantScreenMixin.java
@@ -2,6 +2,7 @@ package com.natamus.wanderingtradermayleave.mixin;
 
 import com.natamus.wanderingtradermayleave.util.Util;
 import net.minecraft.client.gui.screens.inventory.MerchantScreen;
+import net.minecraft.network.chat.contents.TranslatableContents;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,6 +12,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MerchantScreenMixin {
     @Inject(method = "init()V", at = @At(value = "TAIL"))
     protected void init(CallbackInfo ci) {
-        Util.addLeaveButton((MerchantScreen)(Object)this);
+        MerchantScreen merchantScreen = (MerchantScreen)(Object)this;
+		if (merchantScreen.getTitle().getContents() instanceof TranslatableContents contents) {
+            if (contents.getKey().equals("entity.minecraft.wandering_trader")) {
+                Util.addLeaveButton(merchantScreen);
+            }
+        }
     }
 }
--- Common/src/main/java/com/natamus/wanderingtradermayleave/util/Reference.java
+++ Common/src/main/java/com/natamus/wanderingtradermayleave/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.wanderingtradermayleave.util;
 public class Reference {
 	public static final String MOD_ID = "wanderingtradermayleave";
 	public static final String NAME = "Wandering Trader May Leave";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 	public static final String ACCEPTED_VERSIONS = "[1.21.5]";
 }
