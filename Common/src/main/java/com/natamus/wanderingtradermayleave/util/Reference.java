ef0e09b877eb54a1035177e177a95edc7df5322f
Rick South <rick@natamus.com>
2025-10-08 00:48:49 +0200

    Wandering Trader May Leave source code for 1.21.10.

---
+++ Common/src/main/java/com/natamus/wanderingtradermayleave/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.wanderingtradermayleave.util;
+
+public class Reference {
+	public static final String MOD_ID = "wanderingtradermayleave";
+	public static final String NAME = "Wandering Trader May Leave";
+	public static final String VERSION = "1.1";
+	public static final String ACCEPTED_VERSIONS = "[1.21.10]";
+}
